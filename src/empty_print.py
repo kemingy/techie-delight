3cbe00d952f1373c78588c154fbe18d3fc67580a
keming <kemingy94@gmail.com>
2018-08-19 19:43:07 +0800

    empty print

---
+++ src/empty_print.py
@@ -0,0 +1,11 @@
+# Write a program to print "Hello World" with empty main function i.e. without
+# writing anything inside main() function.
+
+class Empty:
+    def __init__(self):
+        print('Hello World')
+
+x = Empty()
+
+if __name__ == '__main__':
+    pass
