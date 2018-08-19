c311c8fa785a98abd419e2511d60f95605b318fe
keming <kemingy94@gmail.com>
2018-08-19 19:54:29 +0800

    print semicolon

---
+++ src/print_semicolon.py
@@ -0,0 +1,8 @@
+# Write a program to print a semicolon without using semicolon anywhere in the
+# program.
+
+def print_semicolon():
+    print('{}'.format(chr(59)))
+
+if __name__ == '__main__':
+    print_semicolon()
